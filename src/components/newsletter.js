4f5d13ba46711676a02e760cdf86a814b96ccbe2
Luis Antonio de Souza Silva <dev.luis2003@gmail.com>
2023-09-16 19:31:59 -0300

    feat: creating newsletter component, mixins for the button and input elements

---
+++ src/components/newsletter.js
@@ -0,0 +1,33 @@
+class Newsletter extends HTMLElement {
+  constructor() {
+    super();
+  }
+
+  connectedCallback() {
+    this.innerHTML = `
+      <section class="newsletter">
+        <div class="newsletter-infos">
+          <h2>
+            <i class="fa-solid fa-envelope-circle-check"></i>
+            Receba as novidades
+          </h2>
+          <p>Inscreva-se e receba as novidades no seu e-mail.</p>
+        </div>
+
+        <form>
+          <div class="newsletter-form-group">
+            <input type="text" placeholder="Insira seu e-mail" class="newsletter-input">
+            <button class="newsletter-button" type="button">Assinar agora</button>
+          </div>
+
+          <div class="newsletter-form-group">
+            <input type="checkbox" class="newsletter-checkbox" id="newsletter-check">
+            <label class="newsletter-checkbox-label" for="newsletter-check">Concordo em receber comunicações</label>
+          </div>
+        </form>
+      </section>
+    `;
+  }
+}
+
+customElements.define("newsletter-component", Newsletter);
