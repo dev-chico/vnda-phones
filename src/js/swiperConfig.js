48d2c220da2bceed51a0322b83752fca0f31c36d
Luis Antonio de Souza Silva <dev.luis2003@gmail.com>
2023-09-19 16:05:04 -0300

    styles: fix responsive slider products

--- src/js/swiperConfig.js
+++ src/js/swiperConfig.js
@@ -3,12 +3,27 @@ const swiperHero = new Swiper(".swiper-hero", {
     delay: 2500,
     disableOnInteraction: false,
   },
+  loop: true,
 });
 
 const swiperNewProducts = new Swiper(".swiper-products", {
-  slidesPerView: 4,
-  spaceBetween: 24,
+  slidesPerView: 1,
+  spaceBetween: 10,
   pagination: {
     el: ".swiper-pagination",
+  },
+  breakpoints: {
+    640: {
+      slidesPerView: 2.5,
+      spaceBetween: 20,
+    },
+    768: {
+      slidesPerView: 3,
+      spaceBetween: 24,
+    },
+    1024: {
+      slidesPerView: 4,
+      spaceBetween: 24,
+    },
   },
 });
